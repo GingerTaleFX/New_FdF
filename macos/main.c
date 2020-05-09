82f834f19f83697e640a0289650ce04221203057
GinerTaleFX <redishtale@gmail.com>
2020-05-09 13:21:20 +0200

    testing macos

--- macos/main.c
+++ macos/main.c
@@ -25,17 +25,27 @@ int				main(int ac, char **av)
 	window = NULL;
 	if (ac == 2)
 	{
+		printf("%d\n", "1");
 		if ((fd = open(av[1], O_RDONLY, 0)) <= 0)
 			terminate(ERR_READING);
+		printf("%d\n", "2");
 		map = new_map();
+		printf("%d\n", "3");
 		read_file(av[1], map, &coords_stack);
+		printf("%d\n", "4");
 		if (!(window = new_window(map)))
 			terminate(ERR_WINDOW_INIT);
+		printf("%d\n", "5");
 		structure_to_array(&coords_stack, map);
+		printf("%d\n", "6");
 		window->camera = init_camera_position();
+		printf("%d\n", "7");
 		draw(map, window);
+		printf("%d\n", "8");
 		controls(window);
+		printf("%d\n", "9");
 		mlx_loop(window->mlx_ptr);
+		printf("%d\n", "10");
 	}
 	else
 		terminate(ERR_USAGE);
